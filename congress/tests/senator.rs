5c0f047aad0b3fdedcd8016c0e2c6090ac4ad477
Albert <albert@framework.tools>
2022-03-25 11:54:10 +1030

    leadership broken but changing to msg system

--- congress/tests/senator.rs
+++ congress/tests/senator.rs
@@ -1,6 +1,7 @@
 use std::{sync::Arc, time::Duration};
 
-use congress::{Senator, Peer, RPCNetwork, Request, UserMessage};
+use congress::{Senator, Peer, RPCNetwork, Request, RPC};
+use futures::join;
 use serde::{Deserialize, Serialize};
 use tokio::{io::{duplex, DuplexStream}, time::{Instant, timeout}};
 
@@ -30,14 +31,14 @@ async fn test_timers() {
 async fn can_create_rpc_network() {
     let ( a_to_b_stream, b_to_a_stream) = duplex(128);
 
-    let rpc_a = RPCNetwork::new();
-    let rpc_b = RPCNetwork::new();
+    let rpc_a = RPCNetwork::new(1);
+    let rpc_b = RPCNetwork::new(2);
 
-    rpc_a.add_peer(Peer::new(2, a_to_b_stream)).await;
-    rpc_b.add_peer(Peer::new(1, b_to_a_stream)).await;
+    rpc_a.add_peer(Peer::new(1, 2, a_to_b_stream)).await.unwrap();
+    rpc_b.add_peer(Peer::new(1, 1, b_to_a_stream)).await.unwrap();
 
-    let a: Arc<SenatorType> = Senator::new(1, rpc_a); // peer 1
-    let b: Arc<SenatorType> = Senator::new(2, rpc_b); // peer 2
+    let a: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_a); // peer 1
+    let b: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_b); // peer 2
 
     a.start();
     b.start();
@@ -55,14 +56,14 @@ async fn can_create_rpc_network() {
 async fn gets_role_updates(){
     let ( a_to_b_stream, b_to_a_stream) = duplex(128);
 
-    let rpc_a = RPCNetwork::new();
-    let rpc_b = RPCNetwork::new();
+    let rpc_a = RPCNetwork::new(1);
+    let rpc_b = RPCNetwork::new(2);
 
-    rpc_a.add_peer(Peer::new(2, a_to_b_stream)).await;
-    rpc_b.add_peer(Peer::new(1, b_to_a_stream)).await;
+    rpc_a.add_peer(Peer::new(1, 2, a_to_b_stream)).await.unwrap();
+    rpc_b.add_peer(Peer::new(1, 1, b_to_a_stream)).await.unwrap();
 
-    let a: Arc<SenatorType> = Senator::new(1, rpc_a); // peer 1
-    let b: Arc<SenatorType> = Senator::new(2, rpc_b); // peer 2
+    let a: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_a); // peer 1
+    let b: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_b); // peer 2
 
     let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
 
@@ -92,14 +93,14 @@ async fn gets_role_updates(){
 async fn can_push_custom_messages(){
     let ( a_to_b_stream, b_to_a_stream) = duplex(128);
 
-    let rpc_a = RPCNetwork::new();
-    let rpc_b = RPCNetwork::new();
+    let rpc_a = RPCNetwork::new(1);
+    let rpc_b = RPCNetwork::new(2);
 
-    rpc_a.add_peer(Peer::new(2, a_to_b_stream)).await;
-    rpc_b.add_peer(Peer::new(1, b_to_a_stream)).await;
+    rpc_a.add_peer(Peer::new(1, 2, a_to_b_stream)).await.unwrap();
+    rpc_b.add_peer(Peer::new(1, 1, b_to_a_stream)).await.unwrap();
 
-    let a: Arc<SenatorType> = Senator::new(1, rpc_a); // peer 1
-    let b: Arc<SenatorType> = Senator::new(2, rpc_b); // peer 2
+    let a: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_a); // peer 1
+    let b: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_b); // peer 2
 
     let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
 
@@ -107,7 +108,10 @@ async fn can_push_custom_messages(){
 
     b.on_message(move |msg| {
         match msg {
-            UserMessage::Request(MyReq::A) => tx.send(()).unwrap(),
+            Message {
+                msg: MessageType::Request(MyReq::A),
+                .
+            } => tx.send(()).unwrap(),
             _ => (),
         }
     }).await;
@@ -116,4 +120,114 @@ async fn can_push_custom_messages(){
     b.start();
 
     timeout(Duration::from_millis(1000), rx.recv()).await.expect("timed out").expect("error receiving");
+}
+
+#[tokio::test]
+async fn deleting_a_peer_works() {
+    let ( a_to_b_stream, b_to_a_stream) = duplex(128);
+
+    let rpc_a = RPCNetwork::new(1);
+    let rpc_b = RPCNetwork::new(2);
+
+    rpc_a.add_peer(Peer::new(1, 2, a_to_b_stream)).await.unwrap();
+    rpc_b.add_peer(Peer::new(1, 1, b_to_a_stream)).await.unwrap();
+
+    let a: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_a.clone()); // peer 1
+    let b: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_b.clone()); // peer 2
+
+    a.start();
+    b.start();
+
+    tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+
+    rpc_a.remove_peer(2).await.unwrap();
+
+    tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+
+    assert!(rpc_a.members().await.len() == 0);
+    assert!(rpc_b.members().await.len() == 0);
+}
+
+
+#[tokio::test]
+async fn duplicate_peers_automatically_resolve() {
+    let ( a_to_b_stream1, b_to_a_stream1) = duplex(128);
+    let ( a_to_b_stream2, b_to_a_stream2) = duplex(128);
+
+    let rpc_a = RPCNetwork::new(1);
+    let rpc_b = RPCNetwork::new(2);
+
+    let peer_1_1_handle = rpc_a.add_peer(Peer::new(1, 2, a_to_b_stream1)).await.unwrap();
+    let peer_2_1_handle = rpc_b.add_peer(Peer::new(1, 1, b_to_a_stream1)).await.unwrap();
+
+    let peer_1_2_handle = rpc_a.add_peer(Peer::new(2, 2, a_to_b_stream2)).await.unwrap();
+    let peer_2_2_handle = rpc_b.add_peer(Peer::new(2, 1, b_to_a_stream2)).await.unwrap();
+
+    assert_eq!(rpc_a.members().await.len(), 1);
+    assert_eq!(rpc_b.members().await.len(), 1);
+
+    let a: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_a.clone()); // peer 1
+    let b: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_b.clone()); // peer 2
+
+    a.start();
+    b.start();
+
+    tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+
+    assert_eq!(rpc_a.members().await.len(), 1);
+    assert_eq!(rpc_b.members().await.len(), 1);
+
+    tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+
+    rpc_a.remove_peer(2).await.unwrap();
+
+    tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+
+    assert_eq!(rpc_a.members().await.len(), 0);
+    assert_eq!(rpc_b.members().await.len(), 0);
+
+    let fut = async move { join!(peer_1_1_handle, peer_1_2_handle, peer_2_1_handle, peer_2_2_handle) };
+
+    let _ = timeout(Duration::from_millis(1000), fut).await.unwrap();
+}
+
+
+#[tokio::test]
+async fn duplicate_peers_automatically_resolve_when_established_by_is_inverted() {
+    let ( a_to_b_stream1, b_to_a_stream1) = duplex(128);
+    let ( a_to_b_stream2, b_to_a_stream2) = duplex(128);
+
+    let rpc_a = RPCNetwork::new(1);
+    let rpc_b = RPCNetwork::new(2);
+
+    let peer_1_1_handle = rpc_a.add_peer(Peer::new(2, 2, a_to_b_stream1)).await.unwrap();
+    let peer_2_1_handle = rpc_b.add_peer(Peer::new(2, 1, b_to_a_stream1)).await.unwrap();
+
+    let peer_1_2_handle = rpc_a.add_peer(Peer::new(1, 2, a_to_b_stream2)).await.unwrap();
+    let peer_2_2_handle = rpc_b.add_peer(Peer::new(1, 1, b_to_a_stream2)).await.unwrap();
+
+
+    let a: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_a.clone()); // peer 1
+    let b: Arc<SenatorType> = Senator::new(Duration::from_millis(1000), rpc_b.clone()); // peer 2
+
+    a.start();
+    b.start();
+
+    tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+
+    assert_eq!(rpc_a.members().await.len(), 1);
+    assert_eq!(rpc_b.members().await.len(), 1);
+
+    tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+
+    rpc_a.remove_peer(2).await.unwrap();
+
+    tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+
+    assert_eq!(rpc_a.members().await.len(), 0);
+    assert_eq!(rpc_b.members().await.len(), 0);
+
+    let fut = async move { join!(peer_1_1_handle, peer_1_2_handle, peer_2_1_handle, peer_2_2_handle) };
+
+    let _ = timeout(Duration::from_millis(1000), fut).await.unwrap();
 }
